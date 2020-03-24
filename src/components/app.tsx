78e11885f6c250fd4157cd246911a632bace490b
MainShayne233 <shaynetremblay@gmail.com>
2020-03-24 16:17:23 -0400

    Basic boilerplate layout

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,9 +6,34 @@ if ((module as any).hot) {
     require("preact/debug");
 }
 
+const styles = {
+    app: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    controls: {
+        border: "solid",
+        borderColor: "red",
+        height: (window.innerHeight * 1) / 8,
+        width: "80%"
+    },
+    editor: {
+        height: "600px",
+        border: "solid",
+        borderColor: "blue",
+        width: "80%",
+        height: (window.innerHeight * 6) / 8
+    }
+};
+
 const App: FunctionalComponent = () => (
-    <div>
-        <p>Home</p>
+    <div style={styles.app}>
+        <div style={styles.controls}></div>
+        <div style={styles.editor}>
+            <canvas />
+        </div>
     </div>
 );
 
