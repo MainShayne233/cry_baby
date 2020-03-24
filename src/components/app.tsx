57d531ed99adafe8e01d3e9a8609d73b778542f0
MainShayne233 <shaynetremblay@gmail.com>
2020-03-24 16:00:49 -0400

    Remove extra bits

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,4 @@
-import { FunctionalComponent, h } from "preact";
-import { Route, Router, RouterOnChangeArgs } from "preact-router";
-
-import Home from "../routes/home";
-import Profile from "../routes/profile";
-import NotFoundPage from '../routes/notfound';
-import Header from "./header";
+import { FunctionalComponent } from "preact";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 if ((module as any).hot) {
@@ -12,23 +6,10 @@ if ((module as any).hot) {
     require("preact/debug");
 }
 
-const App: FunctionalComponent = () => {
-    let currentUrl: string;
-    const handleRoute = (e: RouterOnChangeArgs) => {
-        currentUrl = e.url;
-    };
-
-    return (
-        <div id="app">
-            <Header />
-            <Router onChange={handleRoute}>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
-        </div>
-    );
-};
+const App: FunctionalComponent = () => (
+    <div>
+        <p>Home</p>
+    </div>
+);
 
 export default App;
