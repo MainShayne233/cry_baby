a609b05473fff6967b383475ae4f4a6efbf597e2
MainShayne233 <shaynetremblay@gmail.com>
2020-04-01 21:25:15 -0400

    Get selection page looking good

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,26 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import File exposing (File)
+import File.Select as Select
+import Html exposing (Html, button, div, h1, img, p, text)
+import Html.Attributes exposing (id, src, type_)
+import Html.Events exposing (onClick)
+import Json.Decode as JsonDecode
+import Task
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { imagePath : Maybe String }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { imagePath = Nothing }, Cmd.none )
 
 
 
@@ -23,11 +29,47 @@ init =
 
 type Msg
     = NoOp
+    | PickImage
+    | GotFile File (List File)
+    | GotImagePath String
+    | UseExampleImage
+    | ClearImage
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        PickImage ->
+            ( model
+            , Select.files [ "image/*" ] GotFile
+            )
+
+        GotFile file _ ->
+            ( model
+            , getFileUrl file
+            )
+
+        GotImagePath imagePath ->
+            ( { model | imagePath = Just imagePath }, Cmd.none )
+
+        UseExampleImage ->
+            ( { model | imagePath = Just exampleImagePath }, Cmd.none )
+
+        ClearImage ->
+            ( { model | imagePath = Nothing }, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+getFileUrl : File -> Cmd Msg
+getFileUrl file =
+    Task.perform GotImagePath (File.toUrl file)
+
+
+exampleImagePath : String
+exampleImagePath =
+    "/example_1.jpg"
 
 
 
@@ -36,13 +78,44 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    let
+        page =
+            case model.imagePath of
+                Just imagePath ->
+                    viewEditorPage imagePath
+
+                Nothing ->
+                    viewImageSelectionPage model
+    in
+    div [ id "app-container" ] [ page ]
+
+
+viewEditorPage : String -> Html Msg
+viewEditorPage imagePath =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ img [ src imagePath ] []
+        , button [ onClick ClearImage ] [ text "Clear" ]
+        ]
+
+
+viewImageSelectionPage : Model -> Html Msg
+viewImageSelectionPage model =
+    div [ id "image-selection-page" ]
+        [ h1 [] [ text "Cry Baby" ]
+        , div [ id "image-selection-controls" ]
+            [ div []
+                [ button [ onClick PickImage ] [ text "Upload Image" ]
+                ]
+            , div [] [ text "Or" ]
+            , div []
+                [ button [ onClick UseExampleImage ] [ text "Use Example" ]
+                ]
+            ]
         ]
 
 
 
+---- DECODERS ----
 ---- PROGRAM ----
 
 
